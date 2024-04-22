40a1e055cf7679897320876e69e74204414c3649
Pgonzales <83090342+Patrick119@users.noreply.github.com>
2024-04-22 09:01:43 -0500

    Update index.php
    Se borro el código restante y solo se coloco el código de la conexión con la base de datos

--- xampp/htdocs/stryker/index.php
+++ xampp/htdocs/stryker/index.php
@@ -1,19 +1,5 @@
 <?php
-
-$servidor	= 'localhost';
-$Database 	= 'stryker_database';
-$usuario	= 'root';
-$pass		= '';
-try {
-	$conexion	= mysqli_connect($servidor, $usuario, $pass, $Database);
-
-	if (!$conexion)	{
-		echo '{"codigo":400, "mensaje":"¡Error de conexión!", "respuesta":""}';
-	} else{
-		echo '{"codigo":200, "mensaje":"¡Conexión realizada correctamente!", "respuesta":""}';
-	}
-} catch (Exception $e) {
-	echo '{"codigo":400, "mensaje":"¡Error de conexión!", "respuesta":""}';	
-}
-
-
+$db_servidor	= 'localhost';
+$db_Database 	= 'stryker_database';
+$db_usuario		= 'root';
+$db_pass		= '';
