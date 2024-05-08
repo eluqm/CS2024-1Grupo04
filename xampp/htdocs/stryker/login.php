798691e70f635218f198d4f8e692d71dc823b4c6
Patrick119 <83090342+Patrick119@users.noreply.github.com>
2024-05-07 20:12:29 -0500

    Modificación del login.php
    Se cambio la manera en la que se reciben los datos para la base de datos, se paso de un GET a un POST, esto sirve para que la conexión sea interna desdes el Unity a la base de datos MySql

--- xampp/htdocs/stryker/login.php
+++ xampp/htdocs/stryker/login.php
@@ -2,16 +2,16 @@
 include 'index.php';
 
 try {
-	$conexion	= mysqli_connect($db_servidor, $db_usuario, $db_pass, $db_Database);
+	$conexion = mysqli_connect($db_servidor, $db_usuario, $db_pass, $db_Database);
 
 	if (!$conexion)	{
 		echo '{"codigo":400, "mensaje":"¡Error de conexión!", "respuesta":""}';
 	} else{
 
-		if (isset($_GET['player_name']) && isset($_GET['password'])) {
+		if (isset($_POST['player_name']) && isset($_POST['password'])) {
 
-			$player_name 	= $_GET['player_name'];
-			$password 		= $_GET['password'];
+			$player_name 	= $_POST['player_name'];
+			$password 		= $_POST['password'];
 
 			$sql = "SELECT * FROM `users` WHERE player_name='".$player_name."' and password='".$password."';";
 
@@ -19,22 +19,21 @@ try {
 
 			if ($resultado->num_rows > 0){
 				# Sí existe un usuario con esos datos
-					$sql = "SELECT * FROM `users` WHERE player_name='".$player_name."';";
-					$resultado = $conexion->query($sql);
-					$texto = '';
-					while ($row = $resultado->fetch_assoc()){
-						$texto = 
-						"{
-						#id#:".$row['id'].", 
-						#player_name#:#".$row['player_name']."#, 
-						#password#:#".$row['password']."#,
-						#player#: ".$row['player'].", 
-						#score#: ".$row['score'].
-						"}";
-					}
+				$sql = "SELECT * FROM `users` WHERE player_name='".$player_name."';";
+				$resultado = $conexion->query($sql);
+				$texto = '';
+				while ($row = $resultado->fetch_assoc()) {
+					$texto = 
+					"{#id#:".$row['id'].
+					",#player_name#:#".$row['player_name'].
+					"#,#password#:#".$row['password'].
+					"#,#player#:".$row['player'].
+					",#score#:".$row['score'].
+					",#chapter#:".$row['chapter'].
+					"}";
+				}
 
 				echo '{"codigo":205, "mensaje":"Inicio de sesión exitoso", "respuesta":"'.$texto.'"}';
-
 			}else{
 				# No existe un usuario con esos datos
 				echo '{"codigo":204, "mensaje":"El usuario o la contraseña son incorrectos", "respuesta":""}';
